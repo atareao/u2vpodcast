38bf7c4489f3de3783e2f0626f9e0383e4c4b860
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2022-11-30 06:48:54 +0100

    restructuing

--- src/routes/main.rs
+++ src/routes/main.rs
@@ -1,41 +1,17 @@
+use axum::{response::IntoResponse, http::StatusCode, Json};
 use sqlx::sqlite::SqlitePool;
-use actix_web::{web, get, HttpResponse, Error, http::StatusCode, error::ErrorBadRequest};
-use serde_json::Value;
 use std::env;
+use crate::models::rss::RSS;
 
-use crate::models::{response::Response, rss::RSS};
 
-#[derive(Debug, serde::Serialize, serde::Deserialize)]
-struct Simple{
-    message: String,
-}
-impl Simple {
-    fn new(message: &str) -> Self{
-        Self{
-            message: message.to_string(),
-        }
-    }
-    fn get_value(&self) -> Value{
-        serde_json::to_value(&self).unwrap()
-    }
-    
+pub async fn root() -> impl IntoResponse{
+    (StatusCode::OK, "Hola")
 }
 
-#[get("/")]
-pub async fn root() -> Result<HttpResponse, Error>{
-    let value = Simple::new("Rust").get_value();
-    let response = Response::new(200, "Ok", value);
-    Ok(HttpResponse::build(StatusCode::OK).body(response.get_json_as_string()))
-}
-
-#[get("/rss")]
-pub async fn rss(pool: web::Data<SqlitePool>) -> Result<HttpResponse, Error>{
+pub async fn rss(pool: web::Data<SqlitePool>) -> impl IntoResponse{
     let title = env::var("TITLE").unwrap();
     let description = env::var("DESCRIPTION").unwrap();
     let url = env::var("URL").unwrap();
     let rss = RSS::new(&title, &description, &url);
-    rss.get_feed(&pool)
-        .await
-        .map(|item| HttpResponse::Ok().body(item))
-        .map_err(|e| ErrorBadRequest(e))
+    (StatusCode::OK, Json(rss))
 }
