ac6788c8a98a0368f79fe471d46f8b0877355746
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2022-12-02 20:35:50 +0100

    update migrations

--- migrations/20221202055613_users.up.sql
+++ migrations/20221202055613_users.up.sql
@@ -1,6 +1,5 @@
 CREATE TABLE IF NOT EXISTS users(
     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    username TEXT NOT NULL,
-    hashed_password TEXT NOT NULL,
-    UNIQUE(username)
+    username TEXT NOT NULL UNIQUE,
+    hashed_password TEXT NOT NULL
 )
