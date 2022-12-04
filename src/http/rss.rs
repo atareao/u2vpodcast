e24e36380d88fcbda5e41bf03891600808d41f6e
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2022-12-04 07:11:30 +0100

    update feed

--- src/http/rss.rs
+++ src/http/rss.rs
@@ -5,13 +5,11 @@ use axum::{
     routing::get,
 };
 use serde::Deserialize;
-use crate::models::episode::Episode;
-use crate::models::channel::Channel;
+use crate::models::{episode::Episode, channel::Channel};
 use rss::{ChannelBuilder, ItemBuilder,
     extension::itunes::ITunesItemExtensionBuilder, 
     EnclosureBuilder, GuidBuilder};
-use super::extractor::AuthUser;
-use super::ApiContext;
+use super::{ApiContext, error};
 
 pub fn router() -> Router {
     Router::new()
@@ -27,46 +25,40 @@ pub struct RSS{
 }
 
 async fn feed(
-    auth_user: AuthUser,
     ctx: Extension<ApiContext>,
     Path(path): Path<String>,
-) -> Result<String>{
-        match read_by_path(auth_user, ctx, path).await{
-            None => Ok("Nothing found".to_string()),
-            Some(channel) => {
-                let episodes = Episode::read_all(&ctx.db).await.unwrap();
-                let mut items = Vec::new();
-                for episode in episodes{
-                    let enclosure = format!("{}/{}.mp3", ctx.config.get_url(), episode.yt_id);
-                    let itunes = ITunesItemExtensionBuilder::default()
-                        .image(Some(episode.image))
-                        .build();
-                    let enclosure = EnclosureBuilder::default()
-                        .url(&enclosure)
-                        .build();
-                    let guid = GuidBuilder::default()
-                        .value(episode.yt_id)
-                        .build();
-                    let item = ItemBuilder::default()
-                        .title(Some(episode.title))
-                        .description(Some(episode.description))
-                        .enclosure(Some(enclosure))
-                        .guid(Some(guid))
-                        .pub_date(Some(episode.published_at.to_string()))
-                        .itunes_ext(Some(itunes))
-                        .build();
-                    items.push(item);
-                }
-                let link = format!("{}/rss", ctx.config.get_url());
-                let channel_builder = ChannelBuilder::default()
-                    .title(&channel.title)
-                    .description(&channel.description)
-                    .link(&link)
-                    .items(items)
-                    .build();
-
-                Ok(channel.to_string())
-            }
+) -> Result<String, error::Error>{
+        let channel = Channel::read_by_path(&ctx.pool, &path).await?;
+        let episodes = Episode::read_all(&ctx.pool).await.unwrap();
+        let mut items = Vec::new();
+        for episode in episodes{
+            let enclosure = format!("{}/{}.mp3", ctx.config.get_url(), episode.yt_id);
+            let itunes = ITunesItemExtensionBuilder::default()
+                .image(Some(episode.image))
+                .build();
+            let enclosure = EnclosureBuilder::default()
+                .url(&enclosure)
+                .build();
+            let guid = GuidBuilder::default()
+                .value(episode.yt_id)
+                .build();
+            let item = ItemBuilder::default()
+                .title(Some(episode.title))
+                .description(Some(episode.description))
+                .enclosure(Some(enclosure))
+                .guid(Some(guid))
+                .pub_date(Some(episode.published_at.to_string()))
+                .itunes_ext(Some(itunes))
+                .build();
+            items.push(item);
         }
+        let link = format!("{}/rss", ctx.config.get_url());
+        let channel_builder = ChannelBuilder::default()
+            .title(&channel.title)
+            .description(&channel.description)
+            .link(&link)
+            .items(items)
+            .build();
+        Ok(channel_builder.to_string())
     }
 
