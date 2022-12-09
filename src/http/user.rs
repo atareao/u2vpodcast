f109f0e4d63404612cb01d9f949990f61c8e52e0
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2022-12-09 06:41:16 +0100

    signup

--- src/http/user.rs
+++ src/http/user.rs
@@ -1,14 +1,22 @@
 use crate::http::ApiContext;
 use argon2::password_hash::SaltString;
 use argon2::{Argon2, PasswordHash};
-use axum::extract::Extension;
-use axum::response::IntoResponse;
-use axum::routing::post;
-use axum::{Json, Router};
+use axum::{
+    Json,
+    Router,
+    routing::{get, post},
+    extract::{Multipart, Extension},
+    response::{Html, Response, IntoResponse},
+    http::StatusCode,
+    body::Empty,
+};
 
-use crate::http::error;
-use crate::http::extractor::AuthUser;
-use crate::models::user::User;
+use std::collections::HashMap;
+use crate::{
+    http::{error, extractor::AuthUser},
+    models::user::User
+};
+use tera::{Tera, Context};
 
 
 pub fn router() -> Router {
@@ -21,6 +29,10 @@ pub fn router() -> Router {
             .put(update_user)
             )
         .route("/api/v1/users/login", post(login_user))
+        .route("/signup",
+            get(get_signup)
+            .post(post_signup)
+        )
 }
 
 #[derive(serde::Deserialize, Default, PartialEq, Eq)]
@@ -141,4 +153,82 @@ async fn verify_password(password: String, password_hash: String) -> Result<(), 
     })
     .await
     .unwrap()
+}
+
+async fn get_signup(
+    ctx: Extension<ApiContext>,
+    t: Extension<Tera>
+) -> impl IntoResponse{
+    let mut context = Context::new();
+    context.insert("title", "Signup");
+    Html(t.render("signup.html", &context).unwrap())
+}
+
+async fn post_signup(
+    ctx: Extension<ApiContext>,
+    multipart: Multipart,
+) -> impl IntoResponse {
+    let data = parse_multipart(multipart)
+        .await
+        .map_err(|err| error_page(&err))?;
+    if let (Some(username), Some(password), Some(confirm_password)) = (
+        data.get("username"),
+        data.get("password"),
+        data.get("confirm_password"),
+    ) {
+        if password != confirm_password {
+            Err(error_page(&error::Error::PasswordsDoNotMatch))
+        }else{
+            match hash_password(password.to_string()).await{
+                Ok(hashed_password) =>{
+                    let user = User::create(&ctx.pool, username, &hashed_password).await.unwrap();
+                    let token = AuthUser {
+                        id: user.id,
+                    }
+                    .to_jwt(&ctx);
+                    Ok(set_token(&token))
+                },
+                Err(_) => Err(error_page(&error::Error::InvalidValue))
+            }
+        }
+    } else {
+        Err(error_page(&error::Error::MissingDetails))
+    }
+}
+
+fn set_token(token: &str) -> impl IntoResponse{
+    Response::builder()
+        .status(StatusCode::SEE_OTHER)
+        .header("Location", "/")
+        .header("Authorization", format!("token {}", token))
+        .body(Empty::new())
+        .unwrap()
+}
+
+pub(crate) async fn parse_multipart(
+    mut multipart: Multipart,
+) -> Result<HashMap<String, String>, error::Error> {
+    let mut map = HashMap::new();
+    while let Some(field) = multipart
+        .next_field()
+        .await
+        .map_err(|_err| error::Error::ReadError)?
+    {
+        let name = field.name().ok_or(error::Error::NoName)?.to_string();
+
+        let data = field
+            .text()
+            .await
+            .map_err(|_| error::Error::InvalidValue)?;
+
+        map.insert(name, data);
+    }
+    Ok(map)
+}
+
+pub(crate) fn error_page(err: &dyn std::error::Error) -> impl IntoResponse {
+    Response::builder()
+        .status(StatusCode::INTERNAL_SERVER_ERROR)
+        .body(format!("Err: {}", err))
+        .unwrap()
 }
