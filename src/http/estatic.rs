0513995a9811bc7b2a9f48dd62e0977f4e722d11
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2022-12-11 14:58:45 +0100

    change static server

--- src/http/estatic.rs
+++ src/http/estatic.rs
@@ -1,42 +1,27 @@
 use axum::{
     Router,
-    extract,
-    routing::get,
-    body::{self, Empty, Full},
+    routing::{get, get_service},
     http::{
         StatusCode,
         header::{self, HeaderValue},
     },
-    response::{Response, IntoResponse},
+    response::IntoResponse,
 };
-use mime_guess;
-use include_dir::{include_dir, Dir};
-
-static STATIC_DIR: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR/audios");
+use tower_http::services::ServeDir;
 
 pub fn router() -> Router {
     Router::new()
-        .route("/media/*path",
-            get(static_path)
-        )
+        .route("/favicon.ico", get(favicon))
+        .nest_service(
+            "/media",
+            get_service(ServeDir::new("./audios"))
+        .handle_error(handle_error))
 }
-
-async fn static_path(extract::Path(path): extract::Path<String>) -> impl IntoResponse {
-    let path = path.trim_start_matches('/');
-    let mime_type = mime_guess::from_path(path).first_or_text_plain();
-
-    match STATIC_DIR.get_file(path) {
-        None => Response::builder()
-            .status(StatusCode::NOT_FOUND)
-            .body(body::boxed(Empty::new()))
-            .unwrap(),
-        Some(file) => Response::builder()
-            .status(StatusCode::OK)
-            .header(
-                header::CONTENT_TYPE,
-                HeaderValue::from_str(mime_type.as_ref()).unwrap(),
-            )
-            .body(body::boxed(Full::from(file.contents())))
-            .unwrap(),
-    }
+async fn favicon() -> impl IntoResponse {
+    let one_pixel_favicon = "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mPk+89QDwADvgGOSHzRgAAAAABJRU5ErkJggg==";
+    let pixel_favicon = base64::decode(one_pixel_favicon).unwrap();
+    ([(header::CONTENT_TYPE, HeaderValue::from_static("image/png"))], pixel_favicon)
+}
+async fn handle_error(_err: tokio::io::Error) -> impl IntoResponse {
+    (StatusCode::INTERNAL_SERVER_ERROR, "Something went wrong...")
 }
