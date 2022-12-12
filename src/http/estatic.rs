d5833878ee2a59662bf09d14cc6ea9afc4a3fbf8
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2022-12-12 06:56:49 +0100

    reorder

--- src/http/estatic.rs
+++ src/http/estatic.rs
@@ -1,26 +1,21 @@
 use axum::{
     Router,
-    routing::{get, get_service},
-    http::{
-        StatusCode,
-        header::{self, HeaderValue},
-    },
+    routing::get_service,
+    http::StatusCode,
     response::IntoResponse,
 };
 use tower_http::services::ServeDir;
 
 pub fn router() -> Router {
     Router::new()
-        .route("/favicon.ico", get(favicon))
         .nest_service(
             "/media",
             get_service(ServeDir::new("./audios"))
         .handle_error(handle_error))
-}
-async fn favicon() -> impl IntoResponse {
-    let one_pixel_favicon = "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mPk+89QDwADvgGOSHzRgAAAAABJRU5ErkJggg==";
-    let pixel_favicon = base64::decode(one_pixel_favicon).unwrap();
-    ([(header::CONTENT_TYPE, HeaderValue::from_static("image/png"))], pixel_favicon)
+        .nest_service(
+            "/assets",
+            get_service(ServeDir::new("./assets"))
+        .handle_error(handle_error))
 }
 async fn handle_error(_err: tokio::io::Error) -> impl IntoResponse {
     (StatusCode::INTERNAL_SERVER_ERROR, "Something went wrong...")
