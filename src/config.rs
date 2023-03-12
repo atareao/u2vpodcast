bb949199c9ec978ec087b3c0ad72435e1a6cb932
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2023-03-12 09:14:29 +0100

    all config in yaml

--- src/config.rs
+++ src/config.rs
@@ -8,6 +8,8 @@ const DEFAULT_PER_PAGE: i64 = 10;
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Configuration{
+    #[serde(default = "get_default_dev")]
+    dev: bool,
     log_level: String,
     db_url: String,
     port: u16,
@@ -21,6 +23,10 @@ pub struct Configuration{
     per_page: i64,
     cookies: String,
     channels: Vec<Channel>
+}
+
+fn get_default_dev() -> bool{
+    return false;
 }
 
 fn get_default_sleep_time() -> u64{
@@ -40,6 +46,9 @@ impl Configuration {
     }
     pub fn get_db_url(&self) -> &str{
         &self.db_url
+    }
+    pub fn get_dev(&self) -> bool{
+        self.dev
     }
     pub fn get_sleep_time(&self) -> u64{
         self.sleep_time
