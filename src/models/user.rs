79ea457430afd2ca4f8ca6817bd49c63faa887cb
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2023-05-15 10:42:55 +0200

    user model

--- src/models/user.rs
+++ src/models/user.rs
@@ -45,12 +45,29 @@ impl User {
             updated_at: row.get("updated_at"),
         }
     }
+    pub async fn create(pool: &SqlitePool, email: &str, password: &str) -> Result<User, sqlx::Error>{
+        let sql = "INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *";
+        query(sql)
+            .bind(email.to_ascii_lowercase())
+            .bind(password)
+            .map(Self::from_row)
+            .fetch_one(pool)
+            .await
+    } 
     pub async fn read(pool: &SqlitePool, id: i64) -> Result<Option<User>, sqlx::Error>{
         let sql = "SELECT * FROM users WHERE id = $1";
         query(sql)
             .bind(id)
             .map(Self::from_row)
             .fetch_optional(pool)
+            .await
+    }
+    pub async fn exists(pool: &SqlitePool, email: &str) -> Result<bool, sqlx::Error>{
+        let sql = "SELECT EXISTS(SELECT 1 FROM users WHERE email = $2)";
+        query(sql)
+            .bind(email.to_ascii_lowercase())
+            .map(|row| -> bool {row.get(1)})
+            .fetch_one(pool)
             .await
     }
 }
