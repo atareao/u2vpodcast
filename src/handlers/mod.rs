8f68d63e667ddac3f1079f7a3a3cc6146bdf55f1
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2024-04-07 08:19:15 +0200

    reorder endpoints

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -31,22 +31,21 @@ pub fn config_services(cfg: &mut web::ServiceConfig) {
                 web::redirect("/", "/app/")
             )
             .configure(web_feed)
-            .service(web::resource("/logout/")
-                .route(web::get().to(logout::get_logout))
-            )
             .service(
-                web::resource("/status/")
-                    .route(web::get().to(status::get_status)))
-
-            .service(
-                web::resource("/login/")
-                    .route(web::post().to(login::post_login))
-            ).service(
                 web::scope("/api")
                     .service(
                         web::scope("/1.0")
                             .service(
                                 web::scope("")
+                                    .service(
+                                        web::resource("/logout/")
+                                            .route(web::get().to(logout::get_logout)))
+                                    .service(
+                                        web::resource("/status/")
+                                            .route(web::get().to(status::get_status)))
+                                    .service(
+                                        web::resource("/login/")
+                                            .route(web::post().to(login::post_login)))
                                     .service(channels::read)
                                     .service(channels::read_with_pagination)
                                     .service(episodes::read_with_pagination)
