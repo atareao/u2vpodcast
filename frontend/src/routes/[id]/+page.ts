4fd27cbe97d473b2b8e997c6140e8b986500db0e
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2024-04-09 07:52:38 +0200

    base_endpoint

--- frontend/src/routes/[id]/+page.ts
+++ frontend/src/routes/[id]/+page.ts
@@ -1,8 +1,9 @@
+import { base_endpoint } from '$lib/global';
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ fetch, params }) => {
     console.log(params.id);
-    const endpoint = `/api/1.0/channels/${params.id}/episodes/`;
+    const endpoint = `${base_endpoint}/api/1.0/channels/${params.id}/episodes/`;
     const request = await fetch(endpoint);
     const episodes = await request.json();
     console.log(episodes);
