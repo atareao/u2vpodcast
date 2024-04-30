b074a7abb7145021ec0b07c15c599b76b315511d
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2024-04-30 20:34:06 +0200

    config

--- src/models/config.rs
+++ src/models/config.rs
@@ -1,7 +1,8 @@
-use sqlx::SqlitePool;
-use super::{Error, Param};
+use serde::Deserialize;
+use tokio::fs::read_to_string;
+use tracing::{info, debug};
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Deserialize)]
 pub struct Config{
     pub title: String,
     pub url: String,
@@ -11,19 +12,17 @@ pub struct Config{
     pub jwt_secret: String,
     pub jwt_expires_in: String,
     pub jwt_maxage: i64,
+    pub secret_key: String,
 }
 
 impl Config {
-    pub async fn load(pool: &SqlitePool) -> Result<Self, Error>{
-        Ok(Self{
-            title: Param::get_title(pool).await?,
-            url: Param::get_url(pool).await?,
-            port: Param::get_port(pool).await?,
-            sleep_time: Param::get_sleep_time(pool).await?,
-            per_page: Param::get_per_page(pool).await?,
-            jwt_secret: Param::get_jwt_secret(pool).await?,
-            jwt_expires_in: Param::get_jwt_expires_in(pool).await?,
-            jwt_maxage: Param::get_jwt_maxage(pool).await?,
-        })
+    pub async fn load() -> Self {
+        info!("load");
+        let content = read_to_string("config.yml")
+            .await
+            .expect("Can't read config file `config.yml`");
+        debug!("Content: {content}");
+        serde_yaml::from_str(&content)
+            .expect("Can't process config file `config.yml`")
     }
 }
