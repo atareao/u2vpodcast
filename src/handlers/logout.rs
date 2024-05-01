1005aeadb3554a36bc45e39dc38abd319220bbf8
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2024-05-01 07:31:12 +0200

    logout

--- src/handlers/logout.rs
+++ src/handlers/logout.rs
@@ -3,25 +3,48 @@ use actix_web::{
         Cookie,
         SameSite
     },
+    web::Json,
+    http::StatusCode,
     HttpResponse,
     Responder,
     HttpRequest,
     http::header,
 };
-use tracing::info;
+use actix_session::Session;
+use tracing::{info, error};
+use crate::models::CustomResponse;
 
-pub async fn get_logout(_req: HttpRequest) -> impl Responder{
+use super::super::utils::USER_ID_KEY;
+
+pub async fn get_logout(session: Session) -> impl Responder{
     info!("get_logout");
-    let cookie = Cookie::build("session_auth", "")
-        .path("/")
-        .secure(true)
-        .http_only(true)
-        .same_site(SameSite::Strict)
-        .finish();
+    match session_user_id(&session).await{
+        Ok(_) => {
+            info!("Logout");
+            Json(CustomResponse::new(
+                StatusCode::OK,
+                "You have succesfully logged out",
+                Some(""),
+            ))
+        },
+        Err(e) => {
+            error!("Error: {}", e);
+            Json(CustomResponse::new(
+                StatusCode::BAD_REQUEST,
+                "We currently have some issues. Kindly try again and ensure you are logged in",
+                Some(""),
+            ))
+        }
+    }
+}
 
-    HttpResponse::SeeOther()
-        .insert_header((header::LOCATION, "/login/"))
-        .cookie(cookie)
-        .finish()
+async fn session_user_id(session: &actix_session::Session) -> Result<i64, String> {
+    match session.get(USER_ID_KEY) {
+        Ok(user_id) => match user_id {
+            None => Err("You are not authenticated".to_string()),
+            Some(id) => Ok(id),
+        },
+        Err(e) => Err(format!("{e}")),
+    }
 }
 
